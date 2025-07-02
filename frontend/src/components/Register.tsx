27e83a59cd3c2b103408564619e98c29170456b6
jm19pa <jm19pa@gmail.com>
2025-07-02 11:47:45 -0400

    back to square one

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 
-// const app_name = 'pocketprofessors.com';
-function buildPath(route: string): string {
-  if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:5000/' + route;
-  } else {
-    return '/' + route; // assumes backend and frontend are hosted together
-  }
+const app_name = 'pocketprofessors.com';
+ function buildPath(route:string) : string
+{
+if (process.env.NODE_ENV != 'development')
+{
+return 'http://' + app_name + ':5000/' + route;
+}
+else
+{
+return 'http://localhost:5000/' + route;
+}
+
 }
 
 function Register(){
