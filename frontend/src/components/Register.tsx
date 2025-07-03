860aab38c3c0445144d0b9cf3c031e9d098f5a38
tyler tran <tyler.tran88@yahoo.com>
2025-07-03 12:00:01 -0700

    took out /api/ from build path

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -5,7 +5,7 @@ function buildPath(route:string) : string
 {
 if (process.env.NODE_ENV != 'development')
 {
-return 'http://' + app_name + '/api/' + route;
+return 'http://' + app_name + '/' + route;
 }
 else
 {
