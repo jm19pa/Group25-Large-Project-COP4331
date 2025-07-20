1b08f2e3ef126ebb906d35bedce1233666ec87aa
andrewc48 <acchambers2004@gmail.com>
2025-07-20 15:20:38 -0400

    Update Nodemailer

--- api.js
+++ api.js
@@ -313,7 +313,7 @@ app.post("/api/Verify", async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       host: 'smtp.gmail.com',
-      port:587,
+      port:443,
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
